75a0679271c085b135df54ad31b2f6cec614e05f
André Lucas <andrelucca99@gmail.com>
2025-06-30 13:22:39 -0300

    feat: add pagina de detalhes

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,18 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react"; {/* lib de icons: npm i lucide-react@0.435.0 */}
+{/* lib de icons: npm i lucide-react@0.435.0 */}
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick })
 {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {
@@ -13,8 +24,11 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick })
             >
               {task.title}
             </button>
-            <button className="bg-slate-400 text-white p-2 rounded-md">
-              <ChevronRightIcon />  
+            <button
+              onClick={() => onSeeDetailsClick(task)}
+              className="bg-slate-400 text-white p-2 rounded-md"
+            >
+              <ChevronRightIcon />
             </button>
             <button
               onClick={() => onDeleteTaskClick(task.id)}
