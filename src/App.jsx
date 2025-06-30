3ab40496094aabce7d5bf1c3fb470fb898e1166f
André Lucas <andrelucca99@gmail.com>
2025-06-30 13:32:16 -0300

    feat: add lista no localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState(
-  [
-    {
-      id: 1,
-      title: "Estudar programação",
-      description: "Estudar programação para se tornar um desenvolvedor full stack.",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Estudar Inglês",
-      description: "Estudar Inglês para se tornar fluente.",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar matemática",
-      description: "Estudar matemática para se tornar um desenvolvedor full stack",
-      isCompleted: false,
-    },
-  ])
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
